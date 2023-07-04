8e5728d068ca2eb374aae72a21160c023eba69d7
maulanamalik <maulanamalik6696@gmail.com>
2023-07-04 14:05:49 +0700

    update4

--- application/views/partial/sidebar.php
+++ application/views/partial/sidebar.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <a href="<?= base_url('dashboard') ?>" class="brand-link">
-    <img src="<?= base_url('assets/images/karisma.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <img src="<?= base_url('assets/images/Karisma.png') ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Purchase Order</span>
   </a>
   <!-- Sidebar -->
