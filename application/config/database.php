45321b192f51707658869ab57186e83f90383bcb
maulanamalik <maulanamalik6696@gmail.com>
2023-08-30 08:28:15 +0700

    update_po30082023

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'kiucoid_po',
+// 	'password' => 'kiupokarisma',
+// 	'database' => 'kiucoid_po',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'kiucoid_po',
-	'password' => 'kiupokarisma',
-	'database' => 'kiucoid_po',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'db_preorder',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
